56e05da607115c1852acf0fcb5b6f7e6a9e669de
mauroporras <167700+mauroporras@users.noreply.github.com>
2021-11-24 22:17:04 -0500

    feat: add Zea Engine and Zea Kinematics

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Scene,
+  GLRenderer,
+  TreeItem,
+  Material,
+  GeomItem,
+  Cuboid,
+  NumberParameter,
+  Color,
+  Xfo,
+  Vec3,
+  // @ts-ignore
+} from "@zeainc/zea-engine";
+// @ts-ignore
+import { AttachmentConstraint } from "@zeainc/zea-kinematics";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import "./App.css";
+
+const setupAnimation = (scene: Scene) => {
+  const treeItem = new TreeItem("tree");
+  scene.getRoot().addChild(treeItem);
+  const timeParam = new NumberParameter("time", 0);
+  treeItem.addParameter(timeParam);
+
+  const cube1Material = new Material("cube1Material", "SimpleSurfaceShader");
+  cube1Material.getParameter("BaseColor").setValue(new Color(1, 0, 0));
+  const cube1Item = new GeomItem(
+    "aim",
+    new Cuboid(0.1, 0.2, 0.3),
+    cube1Material
   );
+  treeItem.addChild(cube1Item);
+
+  const cube2Material = new Material("cube2Material", "SimpleSurfaceShader");
+  cube2Material.getParameter("BaseColor").setValue(new Color(0, 1, 0));
+  const cube2Item = new GeomItem(
+    "aim",
+    new Cuboid(0.1, 0.2, 0.3),
+    cube2Material
+  );
+  treeItem.addChild(cube2Item);
+
+  const cube3Material = new Material("cube3Material", "SimpleSurfaceShader");
+  cube3Material.getParameter("BaseColor").setValue(new Color(0, 0, 1));
+  const cube3Item = new GeomItem(
+    "aim",
+    new Cuboid(0.1, 0.2, 0.3),
+    cube3Material
+  );
+  treeItem.addChild(cube3Item);
+
+  const cube1Xfo = new Xfo();
+  cube1Xfo.tr.set(-1, 0, 0);
+  cube1Item.getParameter("LocalXfo").setValue(cube1Xfo);
+
+  const cube2Xfo = new Xfo();
+  cube2Xfo.tr.set(0.5, 0, 0);
+  cube2Item.getParameter("LocalXfo").setValue(cube2Xfo);
+
+  const sttachmentConstraint = new AttachmentConstraint("AimOp");
+  sttachmentConstraint.getInput("Time").setParam(timeParam);
+  sttachmentConstraint
+    .getOutput("Attached")
+    .setParam(cube3Item.getParameter("GlobalXfo"));
+  sttachmentConstraint.addAttachTarget(cube1Item.getParameter("GlobalXfo"), 0);
+  sttachmentConstraint.addAttachTarget(
+    cube2Item.getParameter("GlobalXfo"),
+    2000
+  );
+  sttachmentConstraint.addAttachTarget(
+    cube1Item.getParameter("GlobalXfo"),
+    4000
+  );
+
+  let time = 0;
+  setInterval(() => {
+    time += 20;
+    timeParam.setValue(time);
+
+    cube1Xfo.ori.setFromAxisAndAngle(new Vec3(0, 0, 1), time * 0.001 * Math.PI);
+    cube1Item.getParameter("LocalXfo").setValue(cube1Xfo);
+
+    cube2Xfo.ori.setFromAxisAndAngle(
+      new Vec3(0, 0, 1),
+      time * -0.001 * Math.PI
+    );
+    cube2Item.getParameter("LocalXfo").setValue(cube2Xfo);
+
+    if (time > 6000) time = 0;
+  }, 20);
+};
+
+class App extends React.Component {
+  private canvas: React.RefObject<HTMLCanvasElement>;
+
+  constructor(props: any) {
+    super(props);
+
+    this.canvas = React.createRef();
+  }
+
+  componentDidMount() {
+    const scene = new Scene();
+
+    const renderer = new GLRenderer(this.canvas.current);
+
+    renderer.setScene(scene);
+
+    scene.setupGrid(10, 10);
+
+    setupAnimation(scene);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <canvas ref={this.canvas} />
+      </div>
+    );
+  }
 }
 
 export default App;
